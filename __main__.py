f55e53f4da1f62395a96b9d82c60925ac9406fb7
Logi Leifsson <logileifs@gmail.com>
2021-07-06 22:01:31 +0000

    add nginx service

--- __main__.py
+++ __main__.py
@@ -2,10 +2,12 @@
 import pulumi
 
 import pulumi_digitalocean as do
-#from pulumi_digitalocean import Region
-#from pulumi_digitalocean import DropletSlug
-from pulumi_digitalocean import KubernetesCluster
-from pulumi_digitalocean import KubernetesNodePool
+from pulumi_kubernetes import Provider
+from pulumi_kubernetes.core import v1 as core
+from pulumi_kubernetes.apps.v1 import Deployment
+from pulumi_kubernetes.apps.v1 import DeploymentSpecArgs
+from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
+
 
 config = pulumi.Config()
 region = config.require('region')
@@ -19,20 +21,64 @@ db = do.Droplet(
 	name=db['name'],
 	size=db['size']
 )
-#node_pool = KubernetesNodePool(
+#node_pool = do.KubernetesNodePool(
 #	name='workers',
 #	size=DropletSlug.DROPLET_S1_VCPU2_GB,
 #	node_count=3
 #)
 
-cluster = KubernetesCluster(
+cluster = do.KubernetesCluster(
 	cluster['name'],
 	name=cluster['name'],
 	region=region,
 	version=cluster['version'],
-	node_pool=cluster['base_node_pool']
+	node_pool=do.KubernetesClusterNodePoolArgs(
+		name=cluster['base_node_pool']['name'],
+		size=cluster['base_node_pool']['size'],
+		node_count=cluster['base_node_pool']['node_count']
+	)
 )
 kube_config = cluster.kube_configs[0].raw_config.apply(lambda x: x)
+k8s_provider = Provider("do-k8s", kubeconfig=kube_config)
+
+app_labels = {"app": "app-nginx"}
+app = Deployment(
+	'do-app-dep',
+	spec=DeploymentSpecArgs(
+		selector=LabelSelectorArgs(match_labels=app_labels),
+		replicas=1,
+		template=core.PodTemplateSpecArgs(
+			metadata=ObjectMetaArgs(labels=app_labels),
+			spec=core.PodSpecArgs(
+				containers=[
+					core.ContainerArgs(name='nginx', image='nginx')
+				]
+			),
+		),
+	),
+	opts=pulumi.ResourceOptions(provider=k8s_provider)
+)
+
+ingress = core.Service(
+	'do-app-svc',
+	spec=core.ServiceSpecArgs(
+		type='LoadBalancer',
+		selector=app_labels,
+		ports=[core.ServicePortArgs(port=80)],
+	),
+	opts=pulumi.ResourceOptions(
+		provider=k8s_provider,
+		custom_timeouts=pulumi.CustomTimeouts(
+			create="15m",
+			delete="15m"
+		)
+	)
+)
+
+ingress_ip = ingress.status.apply(lambda s: s.load_balancer.ingress[0].ip)
+
+
 pulumi.export('db', db)
 pulumi.export('cluster', cluster)
+pulumi.export('ingress_ip', ingress_ip)
 pulumi.export('kube_config', kube_config)
